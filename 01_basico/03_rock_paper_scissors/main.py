2d08c113331aea015089a739b167595978580a71
strayb <strayb.null@gmail.com>
2026-04-24 18:12:17 -0300

    feat(03_rock_paper_scissors): Cria janela principal

--- 01_basico/03_rock_paper_scissors/main.py
+++ 01_basico/03_rock_paper_scissors/main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from 03-rock-paper-scissors!")
+from tkinter import *
+import random
+
+
+#Paleta
+bg_color = "#FFFFFF"
+
+#janela principal
+root = Tk()
+root.geometry('400x400')
+root.resizable(0,0)
+root.title("Pedra, Papel, Tesoura")
+root.config(bg = bg_color)
+
+# Label Título
+Label(
+    root, 
+    text="Pedra, Papel, Tesoura", 
+    font=("Roboto", 15), 
+    bg=bg_color 
+).pack(pady=10)
+
 
 
 if __name__ == "__main__":
-    main()
+    root.mainloop()
