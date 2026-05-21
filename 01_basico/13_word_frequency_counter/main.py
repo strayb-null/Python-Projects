c46f8bb9474962a8c57ea1ff83bb3d39a964fee8
strayb <strayb.null@gmail.com>
2026-05-21 03:33:25 -0300

    feat(13_word_frequency_counter): implementa leitura e tokenizacao de texto
    add clean_and_tokenize() com normalizacao, remocao de pontuacao e stop words

--- 01_basico/13_word_frequency_counter/main.py
+++ 01_basico/13_word_frequency_counter/main.py
@@ -1,6 +1,42 @@
-def main():
-    print("Hello from 13-word-frequency-counter!")
+import string
+import os 
+
+def read_file(filepath):
+    try:
+        with open(filepath, "r", encoding="utf-8"):
+            content = filepath.read()
+            return content
+    except FileNotFoundError:
+        print(f"[ERRO] Arquivo '{filepath}' não encontrado.")
+        return None
+    except Exception as e:
+        print(f"[ERRO] Falha ao ler o arquivo: {e}")
+        return None
+    
+def clean_and_tokenize(text: str):
+    text = text.lower()
+
+    for char in string.punctuation:
+        text = text.replace(char, ' ')
+    
+    words = text.split()
+
+    stop_words = {
+        'a', 'o', 'e', 'de', 'do',
+        'da', 'em', 'um', 'uma',
+        'que', 'para', 'com', 'os',
+        'as', 'no', 'na', 'se',
+    }
+
+    clean_words = []
+    for word in words:
+        if word not in stop_words:
+            clean_words.append(word)
+
+    return clean_words
+
 
 
 if __name__ == "__main__":
-    main()
+    pass
+
--- README.md
+++ README.md
@@ -63,7 +63,9 @@ Cada pasta representa um projeto independente, geralmente pequeno e focado em um
 | 09 | [`09_fun_facts_generator`](./01_basico/09_fun_facts_generator/) | simples gerador de curiosidades | ✅ |
 | 10 | [`10_pdf_receipt_generator`](./01_basico/10_pdf_receipt_generator/) | simples gerador de recibo | ✅ |
 | 11 | [`11_countdown_timer`](./01_basico/11_countdown_timer/) | simples countdown timer| ✅ |
-| 11 | [`12_gerador_senhas`](./01_basico/12_gerador_senhas/) | simples gerador de senhas| ✅ |
+| 12 | [`12_gerador_senhas`](./01_basico/12_gerador_senhas/) | simples gerador de senhas| ✅ |
+| 13 | [`13_word_frequency_counter`](./01_basico/13_word_frequency_counter/) | Contador de frequência| 🚧 |
+
 > **Legenda:** ✅ Concluído · 🚧 Em andamento · 📋 A fazer
 
 ---
