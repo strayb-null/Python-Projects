de45b232d823ca81173a860e24fdd87fcd8dc97b
strayb <strayb.null@gmail.com>
2026-05-22 15:40:07 -0300

    feat(tile): implementa classe Tile com renderização e cálculo de posicionamento

--- 01_basico/14_2048_game/main.py
+++ 01_basico/14_2048_game/main.py
@@ -24,6 +24,50 @@ MOVE_VEL = 20
 WINDOW = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("2048")
 
+class Tile:
+    COLORS = [
+        (237, 229, 218),
+        (238, 225, 201),
+        (243, 178, 122),
+        (246, 150, 101),
+        (247, 124, 95),
+        (247, 95, 59),
+        (237, 208, 115),
+        (237, 204, 99),
+        (236, 202, 80),
+    ]
+
+    def __init__(self, value, row, col):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.x = col * RECT_WIDTH
+        self.y = row * RECT_HEIGHT
+
+    def get_color(self):
+        color_index = int(math.log2(self.value)) - 1
+        color = self.COLORS[color_index]
+        return color
+    
+    def draw(self, window):
+        color = self.get_color()
+        pygame.draw.rect(window, color, (self.x, self.y, RECT_WIDTH, RECT_HEIGHT))
+
+        text = FONT.render(str(self.value), 1, FONT_COLOR)
+        window.blit(
+            text,
+            (self.x + (RECT_WIDTH / 2 - text.get_width() / 2),
+             self.y + (RECT_HEIGHT / 2 - text.get_height() / 2),
+            )
+        )
+
+    def set_pos(self):
+        pass
+
+    def move(self, delta):
+        pass
+
+
 def draw_grid(window):
     for row in range(1, ROWS):
         y = row * RECT_HEIGHT
