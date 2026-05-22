120190b0d5374607843fabd8267b35d28c55cea7
strayb <strayb.null@gmail.com>
2026-05-21 22:39:35 -0300

    feat(14_2048_game): adiciona funções de drawing do tabuleiro

--- 01_basico/14_2048_game/main.py
+++ 01_basico/14_2048_game/main.py
@@ -24,6 +24,25 @@ MOVE_VEL = 20
 WINDOW = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("2048")
 
+def draw_grid(window):
+    for row in range(1, ROWS):
+        y = row * RECT_HEIGHT
+        pygame.draw.line(window, OUTLINE_COLOR, (0, y), (WIDTH, y), OUTLINE_THICKNESS)
+
+    for col in range(1, COLS):
+        x = col * RECT_WIDTH
+        pygame.draw.line(window, OUTLINE_COLOR, (x, 0), (x, HEIGHT), OUTLINE_THICKNESS)
+
+    pygame.draw.rect(window, OUTLINE_COLOR, (0,0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
+
+def draw(window):
+    window.fill(BACKGROUND_COLOR)
+    
+    draw_grid(window)
+
+    pygame.display.update()
+
+
 def main(window):
     clock = pygame.time.Clock()
     run = True
@@ -35,6 +54,8 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+        
+        draw(window)
 
     pygame.quit()
 
