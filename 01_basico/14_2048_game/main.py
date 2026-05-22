d9c9d8e4fbce3438f419f145a9a741b459c653cc
strayb <strayb.null@gmail.com>
2026-05-21 21:52:40 -0300

    feat(14_2048_game): configura interface gráfica e constantes iniciais
    - Inicializa pygame e cria janela 800x800
    - Define constantes do grid (4x4), cores e tipografia
    - Configura FPS e velocidade de movimento

--- 01_basico/14_2048_game/main.py
+++ 01_basico/14_2048_game/main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from 14-2048-game!")
+import pygame
+import random
+import math
 
+pygame.init()
 
-if __name__ == "__main__":
-    main()
+FPS = 60
+
+WIDTH, HEIGHT = 800, 800
+ROWS = 4
+COLS = 4
+
+RECT_HEIGHT = HEIGHT // ROWS
+RECT_WIDTH = WIDTH // COLS
+
+OUTLINE_COLOR = (187,173, 160)
+OUTLINE_THICKNESS = 10
+BACKGROUND_COLOR = (205, 192, 180)
+FONT_COLOR = (119, 110, 101)
+
+WINDOW = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("2048")
+FONT = pygame.font.SysFont("comicsans", 60, bold=True)
+MOVE_VEL = 20
