f61f13cc5ea402ada9377f382bd3d5d484ff12ea
strayb <strayb.null@gmail.com>
2026-05-21 22:21:10 -0300

    feat(14_2048_game): implementa loop principal e tratamento de eventos
    - Adiciona função main com o game loop
    - Configura controle de FPS usando pygame.time.Clock
    - Implementa escuta de eventos para encerrar o jogo (pygame.QUIT)

--- 01_basico/14_2048_game/main.py
+++ 01_basico/14_2048_game/main.py
@@ -18,7 +18,25 @@ OUTLINE_THICKNESS = 10
 BACKGROUND_COLOR = (205, 192, 180)
 FONT_COLOR = (119, 110, 101)
 
-WINDOW = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("2048")
 FONT = pygame.font.SysFont("comicsans", 60, bold=True)
 MOVE_VEL = 20
+
+WINDOW = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("2048")
+
+def main(window):
+    clock = pygame.time.Clock()
+    run = True
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main(WINDOW)
