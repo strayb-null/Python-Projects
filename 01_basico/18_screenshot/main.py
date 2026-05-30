a8c29a9d13d44c53a6f393cf178034a46370823a
strayb <strayb.null@gmail.com>
2026-05-30 20:19:41 -0300

    feat(18_screenshot): Implementa o codigo completo
    docs(README Principal): adiciona projeto como concluído (✅)

--- 01_basico/18_screenshot/main.py
+++ 01_basico/18_screenshot/main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from 18-screenshot!")
+import pyscreenshot as ImageGrab
+from datetime import datetime
+import os
 
+def tirar_print():
+    # Cria uma pasta se não existir
+    os.makedirs("screenshots", exist_ok=True)
+    
+    # Nome do arquivo com data e hora
+    nome_arquivo = datetime.now().strftime("screenshots/print_%d-%m-%Y_%H-%M-%S.png")
+    
+    image = ImageGrab.grab()
+    image.save(nome_arquivo)
+    print(f"Screenshot salvo como: {nome_arquivo}")
 
-if __name__ == "__main__":
-    main()
+tirar_print()
