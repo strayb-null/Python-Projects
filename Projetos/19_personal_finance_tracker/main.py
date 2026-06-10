5173f8c1b803bd7abfaf61c03ee2aec03b7ff31f
strayb <strayb.null@gmail.com>
2026-06-10 18:14:57 -0300

    feat(19_personal_finance_tracker): implementa classe CSV com inicialização e adição de entradas

--- Projetos/19_personal_finance_tracker/main.py
+++ Projetos/19_personal_finance_tracker/main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from 19-personal-finance-tracker!")
+import csv
+import pandas as pd
+from datetime import datetime
 
+class CSV:
+    ARQUIVO_CSV = "dados_financeiro.csv"
+    COLUMNS = ["data", "valor", "categoria", "descricao"]
 
-if __name__ == "__main__":
-    main()
+    @classmethod
+    def inciar_csv(cls):
+        try:
+            pd.read_csv(cls.ARQUIVO_CSV)
+        except FileNotFoundError:
+            df = pd.DataFrame(columns=cls.COLUMNS)
+            df.to_csv(cls.ARQUIVO_CSV, index=False)
+
+    @classmethod
+    def add_entrada(cls, data, valor, categoria, descricao):
+        nova_entrada = {
+            "data": data,
+            "valor": valor,
+            "categoria": categoria,
+            "descricao" : descricao            
+        }
+        with open(cls.ARQUIVO_CSV, "a", newline="") as arquivocsv:
+            writer = csv.DictWriter(arquivocsv, fieldnames=cls.COLUMNS)
+            writer.writerow(nova_entrada)
+        print("Entrada adicionada com sucesso!")
+
+CSV.inciar_csv()
+CSV.add_entrada("Teste", 100, "Teste", "Teste")
