bc9679bda78d2ae5aef044ea86d9e8cfcea45c84
strayb <strayb.null@gmail.com>
2026-06-12 18:24:56 -0300

    feat(19_personal_finance_tracker): implementa get_lancamentos, função add e menu principal

--- Projetos/19_personal_finance_tracker/main.py
+++ Projetos/19_personal_finance_tracker/main.py
@@ -1,10 +1,12 @@
 import csv
 import pandas as pd
 from datetime import datetime
+from data_entry import get_data, get_valor, get_categoria, get_descricao
 
 class CSV:
     ARQUIVO_CSV = "dados_financeiro.csv"
     COLUMNS = ["data", "valor", "categoria", "descricao"]
+    FORMATO = "%d-%m-%Y"
 
     @classmethod
     def inciar_csv(cls):
@@ -27,5 +29,71 @@ class CSV:
             writer.writerow(nova_entrada)
         print("Entrada adicionada com sucesso!")
 
-CSV.inciar_csv()
-CSV.add_entrada("Teste", 100, "Teste", "Teste")
+    @classmethod
+    def get_lancamentos(cls, data_inicial, data_final):
+        df = pd.read_csv(cls.ARQUIVO_CSV)
+        df["data"] = pd.to_datetime(df["data"], format=CSV.FORMATO )
+        data_inicial = datetime.strptime(data_inicial, CSV.FORMATO)
+        data_final = datetime.strptime(data_final, CSV.FORMATO)
+
+        mask = (df["data"] >= data_inicial) & (df["data"] <= data_final)
+        df_filtrado = df.loc[mask]
+
+        if df_filtrado.empty:
+            print("Nenhum lançamento encontrado nesse período")
+        else:
+            print(f"Lançamentos de {data_inicial.strftime(CSV.FORMATO)} até {data_final.strftime(CSV.FORMATO)}")
+            
+            print(df_filtrado.to_string(
+                index=False, formatters={"data": lambda x: x.strftime(CSV.FORMATO)}
+            ))
+
+        ganhos_total = df_filtrado[df_filtrado["categoria"] == "Ganhos"]["valor"].sum()
+        despesas_total = df_filtrado[df_filtrado["categoria"] == "Despesas"]["valor"].sum()
+        print("\nResumo Financeiro")
+        print(f"Total de ganhos: R${ganhos_total:.2f}")
+        print(f"Total de despesas: R${despesas_total:.2f}")
+        print(f"Saldo: R${(ganhos_total - despesas_total):.2f}")
+
+        return df_filtrado
+
+
+def add():
+    CSV.inciar_csv()
+    data = get_data(
+        "Digite a data do lançamento (mm-dd-yyy) ou deixe em branco e aperte enter para inserir a data de hoje: ",
+        permitir_default = True
+    )
+
+    valor = get_valor()
+    categoria = get_categoria()
+    descricao = get_descricao()
+    CSV.add_entrada(data, valor, categoria, descricao)
+
+
+def main():
+    while True:
+        print("\n1. Adicionar nova transação")
+        print("2. Ver transações e resumo dentro de um período")
+        print("3. Sair")
+
+        escolha = input("Escolha uma opção (1-3): ")
+
+        if escolha == "1":
+            add()
+        elif escolha == "2":
+            data_inicial = get_data("Digite a data inicial (dd-mm-yyyy): ")
+            data_final = get_data("Digite a data final (dd-mm-yyyy): ")
+            df = CSV.get_lancamentos(data_inicial, data_final)
+        elif escolha == "3":
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida. Digite 1, 2 ou 3.")
+
+
+if __name__ == "__main__":
+    main()
+
+        
+            
